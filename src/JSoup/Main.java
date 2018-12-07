992c81bb5f30e19de412643393d71f63402e94ea
SirCaramel <przemko.k23@gmail.com>
2018-12-07 15:04:54 +0100

    Formatowanie kodu

--- src/JSoup/Main.java
+++ src/JSoup/Main.java
@@ -31,56 +31,33 @@ public class Main
         int cardQuantity = numberOfTR.size();
 
         Elements[] allTR = new Elements[cardQuantity];
+        String[] relHref = new String[cardQuantity];
+        Connection[] cardSite = new Connection[cardQuantity];
+        Document[] cardSiteDoc = new Document[cardQuantity];
+        Elements[] allDD = new Elements[cardQuantity];
+        Elements[] expansion = new Elements[cardQuantity];
+        String[] priceTrend = new String[cardQuantity];
 
-        for(int i=0;i<allTR.length;i++)
+        for(int i=0;i<numberOfTR.size();i++)
         {
             int j = 1;
             j += i;
 
             allTR[i] = cardSearchDoc.select("#innerNavBarCodeDiv > table > tbody > tr:nth-child(" + j + ") > td:nth-child(5) > a");
-        }
 
-        String[] relHref = new String[cardQuantity];
-
-        for(int i=0;i<relHref.length;i++)
-        {
             relHref[i] = allTR[i].attr("href");
-        }
 
-        Connection[] cardSite = new Connection[cardQuantity];
-        for(int i=0;i<cardSite.length;i++)
-        {
             cardSite[i] = Jsoup.connect("https://www.cardmarket.com" + relHref[i]);
-        }
 
-        Document[] cardSiteDoc = new Document[cardQuantity];
-        for(int i=0;i<cardSiteDoc.length;i++)
-        {
             cardSiteDoc[i] = cardSite[i].get();
-        }
 
-        Elements[] allDD = new Elements[cardQuantity];
-        for(int i=0;i<allDD.length;i++)
-        {
             allDD[i] = cardSiteDoc[i].select("dd");
-        }
 
-        Elements[] expansion = new Elements[cardQuantity];
-        for(int i=0;i<expansion.length;i++)
-        {
             expansion[i] = cardSiteDoc[i].select("#tabContent-info > div > div.col-12.col-lg-6.mx-auto > div > div.info-list-container.col-12.col-md-8.col-lg-12.mx-auto.align-self-start > dl > dd > div > a.mb-2");
-        }
 
-        String[] priceTrend = new String[cardQuantity];
-        for(int i =0;i<priceTrend.length;i++)
-        {
             priceTrend[i] = allDD[i].last().text();
-        }
 
-        for(int i=0;i<cardQuantity;i++)
-        {
             System.out.println(allTR[i].first().text() + ", " + expansion[i].text() + " - " + "Price Trend: " + priceTrend[i].substring(0, priceTrend[i].indexOf("€") + 1));
         }
-
     }
 }
